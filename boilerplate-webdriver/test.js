6fb18aa9b5dd84e366b436ed4ef9635e32b227c7
RomaMhatre <60507672+RomaMhatre@users.noreply.github.com>
2020-01-31 17:01:00 +0530

    Update test.js

--- boilerplate-webdriver/test.js
+++ boilerplate-webdriver/test.js
@@ -1,35 +1,55 @@
-const should = require('chai').should()
-const expect = require('chai').expect
-const wdio = require('webdriverio')
-
 describe('Test', function() {
 	this.timeout(0)
-	var driver = null
-	const options = {
-		hostname: 'localhost',
+	const wd = require('wd')
+	const driver = wd.promiseChainRemote({
+		host: 'localhost',
 		port: 4723,
-		logLevel: 'error',
-		capabilities: {
-			platformName: 'android',
-			deviceName: 'Device',
-			automationName: 'UiAutomator2',
-			app: 'path_to_apk'
-			// TODO: Replace above with local APK file path or 
-			// a link on the internet e.g., https://mq.link/codelab/google_io_19.apk
-		},
-	}
+	})
 
 	before(async () => {
-		driver = await wdio.remote(options)
+		await driver.init({
+			platformName: 'android',
+			app: 'https://firebasestorage.googleapis.com/v0/b/mq-prod.appspot.com/o/files%2Fazularc%2F1576072839925%2FAllison%20Smith.apk?alt=media&token=44a41540-711f-4e8f-b771-c9ee0418663f',
+			deviceName: 'Device',
+			automationName: 'UiAutomator2',
+			appWaitActivity: '*',
+			autoGrantPermissions: true,
+			noReset: false,
+			autoLaunch: false
+		})
+		// Have Appium automatically determine which permissions your app requires and grant them to the app on install.
+		// http://appium.io/docs/en/writing-running-appium/caps/
+		await driver.setImplicitWaitTimeout(15000)
 	})
 
 	after(async () => {
-		await driver.deleteSession()
+		return await driver.quit()
 	})
 
-
-	it('First Test', async () => {
+	it('Login', async () => {
+		await driver.launchApp()
+		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/emailEditText\")").then(target => target.type("tej.mhatre@azularc.com"))
+		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/passwordEditText\")").then(target => target.type("12345678"))
+		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/loginButton\")").then(target => target.click())
+		await new Promise(resolve => setTimeout(resolve, 1000))
 	})
 
-	it('Second Test', async () => {
+	it('Select the project Painting Project', async () => {
+		await driver.elementByAndroidUIAutomator("new UiSelector().text(\"Painting Project\")").then(target => target.click())
+	})
+
+	it('Clock In Tej Mhatre', async () => {
+		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/clockinout\")").then(target => target.click())
+		await driver.elementByAndroidUIAutomator("new UiSelector().text(\"Manual Clock In/Out\")").then(target => target.click())
+		await driver.elementByAndroidUIAutomator("new UiSelector().text(\"Tej Mhatre\")").then(target => target.click())
+	})
+
+	it('Check Project Overview', async () => {
+		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/projectOverview\")").then(target => target.click())
+		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/percentageButton\")").then(target => target.click())
+		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/hoursButton\")").then(target => target.click())
+		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/menuRight\")").then(target => target.click())
+		await driver.elementByAndroidUIAutomator("new UiSelector().text(\"Project Home\")").then(target => target.click())
+	})
+
 })
