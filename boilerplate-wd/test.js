f043a4dc05c2e4e8bbc0b5c090f14d08c6d54784
Pushkar Kolhe <pushkar@moquality.com>
2020-02-19 08:21:20 -0500

    Updates the wd and webdriver boilerplates

--- boilerplate-wd/test.js
+++ boilerplate-wd/test.js
@@ -14,9 +14,9 @@ describe('Test', function() {
 			platformName: 'android',
 			deviceName: 'Device',
 			automationName: 'UiAutomator2',
-			app: 'path_to_apk' 
-			// TODO: Replace above with local APK file path or 
-			// a link on the internet e.g., https://mq.link/codelab/google_io_19.apk
+			app: 'path_to_apk',
+			autoGrantPermissions: true,
+			appWaitActivity: '*'
 		})
 	})
 
--- boilerplate-webdriver/test.js
+++ boilerplate-webdriver/test.js
@@ -13,9 +13,9 @@ describe('Test', function() {
 			platformName: 'android',
 			deviceName: 'Device',
 			automationName: 'UiAutomator2',
-			app: 'path_to_apk'
-			// TODO: Replace above with local APK file path or 
-			// a link on the internet e.g., https://mq.link/codelab/google_io_19.apk
+			app: 'path_to_apk',
+			autoGrantPermissions: true,
+			appWaitActivity: '*'
 		},
 	}
 
@@ -32,4 +32,6 @@ describe('Test', function() {
 	})
 
 	it('Second Test', async () => {
+	})
+
 })
